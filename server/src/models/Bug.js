17a2f8868344889711a274274c03c1ca1a426c89
ynoTL <tonylu23@gmail.com>
2022-02-28 23:50:46 -0500

    fix Bug expirationDate timestamp calculations

--- server/src/models/Bug.js
+++ server/src/models/Bug.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose'
+import moment from 'moment'
 
 const bugsSchema = mongoose.Schema({
   title: { type: String, required: true, trim: true },
   description: { type: String, required: true, trim: true },
-  createdAt: { type: Date, immutable: true, default: () => Date.now() },
-  expirationDate: { type: Date, default: () => Date.now() },
+  createdAt: { type: Date, immutable: true, default: () => moment() },
+  expirationDate: { type: Date, default: () => moment().add(3, 'days') },
   assignee: { type: mongoose.SchemaTypes.ObjectId, ref: 'User' }
 })
 
